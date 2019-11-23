6a15237d3ae5339c12daf0bc4f96397940539e23
Egor Suvorov <egor.suvorov@gmail.com>
2019-11-23 12:56:00 +0300

    largest_heads_run.py: extract get_max_run(flips) function

--- task02-git/largest_heads_run.py
+++ task02-git/largest_heads_run.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
+from typing import List
 import random
+
+
+def get_max_run(flips: List[int]) -> int:
+    cur_run = 0
+    max_run = 0
+    for flip in flips:
+        if flip:
+            cur_run += 1
+            if cur_run > max_run:
+                max_run += 1
+        else:
+            cur_run = 0
+    return max_run
+
 
 ITERS = 1000
 FLIPS = 100
@@ -12,15 +27,7 @@ def main():
     for _ in range(ITERS):
         total += 1
         flips = (random.choice([0, 1]) for _ in range(FLIPS))
-        cur_run = 0
-        max_run = 0
-        for flip in flips:
-            if flip:
-                cur_run += 1
-                if cur_run > max_run:
-                    max_run += 1
-            else:
-                cur_run = 0
+        max_run = get_max_run(flips)
         s += max_run
     print(s, total, s / total)
 
